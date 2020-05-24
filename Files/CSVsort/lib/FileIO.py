10ea93a83a4c7f1587903955f57c3161c23fdf23
Dulatr <dulatr@appstate.edu>
2020-05-24 04:08:57 -0400

    Maintain: Hotfix for data return logic in FileIO module
    Hotfix for item 3 in TODO of commit 0bbce6284b9ab6019bd96896a00e7172b2487dfa.
    
    Changes in commit:
    
    * getHeader()/getContent() methods originally would
      return the columns[1] data for Args.row being True, it
      now returns the rows like intended.
    
    * When rows argument is default, now CSV files with more
      than one column have columns[1:] from getContent()

--- Files/CSVsort/lib/FileIO.py
+++ Files/CSVsort/lib/FileIO.py
@@ -1,7 +1,7 @@
 """
 This module provides interaction with a FileIOwrapper to retrieve data from a CSV file.
 
-All methods expect '\n' and ',' characters seperating date.
+All methods expect '/n' and ',' characters seperating date.
 """
 
 def getHeader(IOwrapper,row=False):
@@ -20,11 +20,11 @@ def getHeader(IOwrapper,row=False):
 
         if row:
             temp = [item.split('\n') for item in content]
-            temp = [item[0].split(',')[0] for item in temp]
-            return temp
+            return temp[0][0].split(',')
         else:
             temp = [item.split('\n') for item in content]
-            return temp[0][0].split(',')
+            temp = [item[0].split(',')[0] for item in temp]
+            return temp
     
     except Exception:
         print("File too short for retrieving header. Empty file?")
@@ -36,17 +36,21 @@ def getContent(IOwrapper,row=False):
     try:
         content = IOwrapper.read().split('\n')
         num_rows = len(content)
-
+        
         if num_rows < 1:
             raise Exception()
         if num_rows == 1:
             return ''     
-        if row:
-            pass
-        else:
-            content.remove(content[0])
         
-        return content
+        # return all but column headers
+        if row:
+            content.remove(content[0])
+            temp = [item.split(',') for item in content]
+            return temp
+
+        # return all columns except row headers
+        temp = [item.split(',')[1:] for item in content]
+        return temp
 
     except Exception:
         print("File too short for retrieving content. Empty file?")
