3b6468ce1d5710604a227c8ccfe5e7667906b9e5
Dulatr <dulatr@appstate.edu>
2020-05-25 10:44:44 -0400

    Maintain: Bring CSVsort to stable with filewrite
    This commit brings project to stable from 0bbce6284b9ab6019bd96896a00e7172b2487dfa
    and increases functionality to intended sorting arguments.
    
    * Remove lib/ references in conjunction with commit baf40c877e6458c4243a2dfec60e7720f2df26c1
    
    * Add Path object for filename checking and prompt
      for overwrite
    
    * Include base error checks for special data cases
      and ensure Field name to sort exists
    
    * Data is now written to a file either with "sorted_"
      prefix or by outfile flag provided
    
    Items 4-6 still remain in list.

--- Files/CSVsort/CSVsort.py
+++ Files/CSVsort/CSVsort.py
@@ -1,26 +1,73 @@
 """
 A CSV record sorting CLI.
 """
-import lib.Parse as p
-from lib.FileIO import *
-import lib.Sort as s
+import Parse as p
+from FileIO import *
+import Sort as s
+from pathlib import Path
 
 args = p.parse()
 
 if not p.checkExt(args):
     raise Exception(f"File name {args.infile[0].name} missing csv extension.")
+if args.o != None:
+    if Path(Path.cwd().__str__() + '/' + args.o.name).is_file():
+        response = input("Outfile name already exists, do you wish to continue? (y/n):> ")
+        if response in ('n','N','no','No','NO'):
+            args.o.close()
+            exit()
 
 # retrieve TextIOwrapper
 infile = args.infile[0]
 
-# store headers
 headers = getHeader(infile,row=args.row)
-print(headers)
+
+if not (args.label[0] in headers):
+    raise ValueError(f"{args.label[0]} not in headers:{headers[:10]}...")
+
 # reset stream position
 infile.seek(0,0)
 
-# store data
 content = getContent(infile,row=args.row)
+infile.close()
 
-print(content)
-print(s.sort(content,1))
+# check base case
+if len(content) == 0:
+    print("No data for fields. Returning...")
+    exit()
+elif len(content[0]) ==1:
+    print("Only one data point per field. Returning...")
+    exit()
+
+if not args.row:
+    content = s.transpose(content)
+    content.sort(reverse=(not args.a),key=lambda data: data[headers.index(args.label[0])])
+    content = s.transpose(content)
+else:
+    content.sort(reverse=(not args.a),key=lambda data: data[headers.index(args.label[0])])    
+
+# recombine elements to string for writing
+content = [','.join(item) for item in content]
+if not args.row:
+    content = [headers[i] + ',' + content[i] for i in range(0,len(headers))]
+    content = '\n'.join(content)
+else:
+    headers = ','.join(headers) + '\n'
+    content = headers + '\n'.join(content)
+
+outfile = '.'
+if args.o is None:
+    outfile = outfile.join(('sorted_' + infile.name).split('.'))
+    print(f'\nNo outfile name provided, saving file as: "{outfile}"...')
+    if Path(Path.cwd().__str__() + '/' + outfile).is_file():
+        response = input("File already exists. Overwrite? (y/n):> ")
+        if response in ('n','N','no','No','NO'):
+            exit()
+    try:
+        with open(outfile,'w') as file:
+            file.write(content)
+    except:
+        print(f"Error opening file '{outfile}' for writing.")
+else:
+    args.o.write(content)
+    args.o.close()
